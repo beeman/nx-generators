5b3ee7cab6d6f9c61194eb23f06e9a6534b2b458
Bram Borggreve <borggreve@gmail.com>
2022-03-08 00:37:40 -0500

    feat: add api-e2e generator

---
+++ tools/generators/api-e2e/index.ts
@@ -0,0 +1,117 @@
+import {
+  addDependenciesToPackageJson,
+  formatFiles,
+  generateFiles,
+  getProjects,
+  getWorkspaceLayout,
+  installPackagesTask,
+  names,
+  Tree,
+  updateJson,
+  updateProjectConfiguration,
+} from '@nrwl/devkit';
+import { join } from 'path';
+import { createEmptyNestApp } from '../lib/helpers/create-empty-nest-app';
+
+export default async function (tree: Tree, schema: any) {
+  const app = schema.app;
+  const name = schema.name || `${app}-e2e`;
+
+  const { npmScope } = getWorkspaceLayout(tree);
+
+  await createEmptyNestApp(tree, name);
+
+  const project = getProjects(tree).get(name);
+
+  // Create files
+  await generateFiles(tree, join(__dirname, 'files'), project.sourceRoot, {
+    ...names(name),
+    app: names(app),
+    npmScope,
+    tmpl: '',
+  });
+
+  // Update project targets
+  updateProjectConfiguration(tree, name, {
+    ...project,
+    targets: {
+      e2e: {
+        executor: '@nrwl/jest:jest',
+        options: {
+          jestConfig: `apps/${name}/jest.config.js`,
+          passWithNoTests: true,
+        },
+      },
+      lint: {
+        executor: '@nrwl/linter:eslint',
+        outputs: ['{options.outputFile}'],
+        options: {
+          lintFilePatterns: [`apps/${name}/**/*.ts`],
+        },
+      },
+    },
+  });
+
+  addDependenciesToPackageJson(
+    tree,
+    {},
+    { '@types/supertest': '2.0.11', supertest: '6.2.2' }
+  );
+
+  // Add path to app in tsconfig.base.json so the api-e2e project can import it
+  const appProject = getProjects(tree).get(app);
+  const appPackage = `@${npmScope}/${app}-app-module`;
+  const appModule = join(appProject.sourceRoot, 'app/app.module.ts');
+
+  updateJson(tree, 'tsconfig.base.json', (source) => {
+    return {
+      ...source,
+      compilerOptions: {
+        ...source.compilerOptions,
+        paths: {
+          [appPackage]: [appModule],
+          ...source.compilerOptions.paths,
+        },
+      },
+    };
+  });
+
+  // Allow api-e2e to import api
+  updateJson(tree, '.eslintrc.json', (source) => {
+    return {
+      ...source,
+      overrides: [
+        ...source.overrides.map((override) => {
+          if (override.rules['@nrwl/nx/enforce-module-boundaries']) {
+            return {
+              ...override,
+              rules: {
+                ...override.rules,
+                '@nrwl/nx/enforce-module-boundaries': [
+                  'error',
+                  {
+                    enforceBuildableLibDependency: true,
+                    allow: [appPackage],
+                    depConstraints: [
+                      {
+                        sourceTag: '*',
+                        onlyDependOnLibsWithTags: ['*'],
+                      },
+                    ],
+                  },
+                ],
+              },
+            };
+          }
+          return override;
+        }),
+      ],
+    };
+  });
+
+  await formatFiles(tree);
+
+  return () => {
+    installPackagesTask(tree);
+  };
+}
