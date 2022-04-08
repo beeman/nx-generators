cefef6be5c4d945103251d239a20b160d52caf4a
Bram Borggreve <borggreve@gmail.com>
2022-04-08 17:32:16 -0500

    feat: add support for creating React apps with Chakra UI

--- tools/generators/react-app/index.ts
+++ tools/generators/react-app/index.ts
@@ -12,21 +12,35 @@ import { Linter } from '@nrwl/linter';
 import { applicationGenerator } from '@nrwl/react';
 import { join } from 'path';
 
-export default async function (tree: Tree, schema: any) {
+function getPackages(ui: 'chakra' | 'tailwind'): Record<string, string> {
+  switch (ui) {
+    case 'chakra':
+      return {
+        '@chakra-ui/react': '^1.8.8',
+        '@chakra-ui/icons': '^1.1.7',
+        '@emotion/react': '^11.9.0',
+        '@emotion/styled': '^11.8.1',
+        'framer-motion': '^6.2.9',
+      };
+    case 'tailwind':
+      return {
+        '@heroicons/react': '^1.0.6',
+        autoprefixer: '^10.4.4',
+        daisyui: '^2.13.6',
+        postcss: '^8.4.12',
+        'react-daisyui': '^1.7.6',
+        tailwindcss: '^3.0.23',
+      };
+  }
+}
+
+export default async function (
+  tree: Tree,
+  schema: { name: string; ui: 'chakra' | 'tailwind' }
+) {
   const name = schema.name;
 
-  addDependenciesToPackageJson(
-    tree,
-    {
-      '@heroicons/react': 'latest',
-      autoprefixer: 'latest',
-      daisyui: 'latest',
-      postcss: 'latest',
-      'react-daisyui': 'latest',
-      tailwindcss: 'latest',
-    },
-    {}
-  );
+  addDependenciesToPackageJson(tree, getPackages(schema.ui), {});
   await applicationGenerator(tree, {
     name,
     style: 'css',
@@ -38,10 +52,15 @@ export default async function (tree: Tree, schema: any) {
   });
   const project = getProjects(tree).get(name);
 
-  await generateFiles(tree, join(__dirname, 'files'), project.sourceRoot, {
-    ...names(name),
-    tmpl: '',
-  });
+  await generateFiles(
+    tree,
+    join(__dirname, 'files', schema.ui),
+    project.sourceRoot,
+    {
+      ...names(name),
+      tmpl: '',
+    }
+  );
 
   updateJson(tree, 'package.json', (val) => ({
     ...val,
